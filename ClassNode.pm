c1efa126ac5581c76886e8d64993bfed8d409bf6
U-DEV\kmurphy <kmurphy@kmurphy2.dev.tricerat.com>
2015-02-09 15:13:18 -0500

    Added adjectives to class parsing

--- ClassNode.pm
+++ ClassNode.pm
@@ -8,7 +8,8 @@ sub new {
      my $class = shift;
      my $self = { name => shift,
 		  body => shift, 
-		  variables => []};
+		  variables => [],
+		  adjectives => []};
      bless $self, $class;
      return $self;
 }
@@ -26,7 +27,17 @@ sub parse {
      	   if ($variable =~ /\s*(\S+)\s+(\S+)\s*?(\[?.*?\]?)$/)
 	   {
      	        print "type:$1 name:$2 attributes:$3\n";
-     	        push($self->{variables}, {type => $1, name => $2, attributes => $3,});
+		my $type = $1;
+	        my $name = $2;
+		my $attributes = $3;
+                if ($type =~ /^adjective$/)
+		{
+		     push($self->{adjectives}, $name);
+		} 	
+		else 
+		{
+		     push($self->{variables}, {type => $1, name => $2, attributes => $3,});
+		}
            }
 	}
      }
