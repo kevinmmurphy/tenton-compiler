a3e91dbf15e3e71f7c28277f251742d814d2d08c
kevinmmurphy <kevinmmurphy@users.noreply.github.com>
2015-08-26 16:15:03 -0400

    Update Cplusplus.pm

--- Cplusplus.pm
+++ Cplusplus.pm
@@ -1,1 +1,21 @@
+#!/usr/bin/perl
+
+package Cplusplus;
+
+sub new {
+     my $class = shift;
+     my $self = {};
+     return bless ($self, $class);
+}
+
+sub Generate {
+     my $self = shift;
+     my $classnode = shift;
+     my @variables = $classnode->GetVariables();
+     my @attributes = $classnode->GetAttributes();
+     my $hname = $classnode->GetName();
+     my $iname = $classnode->GetName();
+     print "Header:$hname Implementation:$iname\n";
+}
+
 1;
