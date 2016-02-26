86744aea728a3210807f72b7d2e5ad85873be641
Kevin Murphy <kevinmmurphy@gmail.com>
2016-02-25 20:58:38 -0500

    Header output initial version C++

--- LangManager.pm
+++ LangManager.pm
@@ -4,6 +4,9 @@ package LangManager;
 
 use ClassNode;
 use Data::Dumper;
+use IO::File;
+
+
 
 sub new {
      my $class = shift;
@@ -18,7 +21,71 @@ sub Generate {
      my @attributes = $classnode->GetAttributes();
      my $hname = $classnode->GetName();
      my $iname = $classnode->GetName();
-     print "Header:$hname Implementation:$iname\n";
+     
+     $iname = $iname . ".cpp";
+     $hname = $hname . ".h";
+
+     $self->GenerateHeader($hname, $classnode);
+     $self->GenerateImplementation($iname, $classnode);
+}
+
+sub GenerateHeader {
+     my $self = shift;
+     my $filename = shift;
+     my $class = shift;
+
+     $fh = IO::File->new();
+     if ($fh->open("> $filename")) {
+          
+          print $fh "#include stdio.h\n";
+          print $fh "\n";
+          print $fh "\n";
+          print $fh "\n";
+          print $fh "\n";
+          print $fh "\n";
+          my $classname = $class->GetName();
+          print $fh "class $classname {\n";
+          print $fh "\n";
+          print $fh "public:\n";
+          #
+          # print the getters and setters
+	  # 
+          my $variables = $class->GetVariables();
+          foreach my $v (@$variables) {
+               my $varname = $v->{name};
+               my $type = $v->{type};
+               
+               print $fh "     void Set$varname\( const $type inarg){ m_$varname = inarg; }\n";
+               print $fh "     $type Get$varname\( void ){ return m_$varname; }\n";
+               print $fh "\n";	
+          }
+
+
+          print $fh "\n";
+          print $fh "private:\n";
+          #
+          # print the member variables
+          # 
+          foreach my $v (@$variables) {
+               my $varname = $v->{name};
+               my $type = $v->{type};
+               
+               print $fh "     $type m_$varname;\n";
+               print $fh "\n";	
+          }
+          print $fh "\n";
+          print $fh "};\n";
+
+	  $fh->close(); 
+     } 
+
+
+}
+
+sub GenerateImplementation {
+
+
+
 }
 
 
