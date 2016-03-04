840ff22704deaf024d9353887b0bf95915c7e64b
Kevin Murphy <kevinmmurphy@gmail.com>
2016-03-03 22:44:33 -0500

    more serializable work

--- Serializable.pm
+++ Serializable.pm
@@ -48,35 +48,20 @@ EOT
 
      }
      print $fh "     writer.writeString(node, retval);\n"; 
-
-#     print $fh "     retval << \'{\'\n";
-#     @methodsstring;
-#     foreach my $v (@$variables) {
-#          my $varname = $v->{name};
-#          my $type = $v->{type};
-#
-#
-#my $line = <<"EOT";
-#     retval << "\\\"$varname\\\":\\\"" << m_$varname << "\\\"";
-#EOT
-#
-#
-#          print $fh $line;
-#
-#     }
-#     print $fh "     retval << \'}\'\n";
-
-
-    
-     print $fh "     return retval;";
-     print $fh "\n}\n";
+     print $fh "     return retval;\n";
+     print $fh "}\n";
      #
      # Define Deserialize method
      #
      print $fh "void $classname\::Deserialize(const std::string &instr) {\n";
      print $fh "     Jzon::Parser parser;\n";
      print $fh "     Jzon::Node object = parser.parseString(instr);\n";
+     print $fh "     if (object.isValid()) {\n";
 
+
+
+     print $fh "     }\n";
+     print $fh "     else { throw; }\n";
 
      print $fh "\n}\n";
 
